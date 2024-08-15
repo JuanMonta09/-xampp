03dc339d25d80c8981e837d5f3758656a6496afa
DdyYazo <alejoyaba@gmail.com>
2024-08-14 22:30:05 -0500

    upd(App) URL base a http://localhost/EjClase/public/

--- app/Controllers/pagina1.php
+++ app/Controllers/pagina1.php
@@ -2,10 +2,10 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController
+class Pagina1 extends BaseController
 {
     public function index(): string
     {
-        return view('pagina');
+        return view('pagina1');
     }
 }
